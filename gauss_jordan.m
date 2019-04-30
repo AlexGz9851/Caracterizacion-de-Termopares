793375eb77ad9fc713e1b5db6947704dbf2cbfcd
Alan Gonzalez <argaalan@hotmail.com>
2019-04-30 14:20:22 -0500

    Resuelve matriz al 90%

--- gauss_jordan.m
+++ gauss_jordan.m
@@ -1,12 +1,58 @@
 matrix = [2, -1, 1; 3, 1, -2; -1, 2, 5]
-fHalfL = 1
-fHalfS = length(matrix)-1
-for i = 1 : 1 : fHalfS
-    for j = length(matrix) : -1 : fHalfL
-        for h = i: 1 : fHalfS
-            %Aqui se resuleve los 0 de la matriz%
+results = [2,9,-5]
+fHalfLongitude = length(matrix)-1
+fHalfHeight = 2
+variableToMultiply = 0
+%Primer for para la primera mitad%
+for i = 1 : 1 : fHalfLongitude %Recorre en horizontal porque primero se resuelve hacia arriba%
+    for j = length(matrix) : -1 : fHalfHeight %Recorre hacia arriba%
+        if matrix(j,i) ~= 0
+            if matrix(j,i) > 0 
+                variableToMultiply = -(matrix(j,i))/(matrix(1,i))
+                if matrix(j,i) + variableToMultiply*matrix(1,i) ~= 0
+                    variableToMultiply = variableToMultiply * -1
+                end
+            else
+                variableToMultiply = (matrix(j,i))/(matrix(1,i))
+                if matrix(j,i) + variableToMultiply*matrix(1,i) ~= 0
+                    variableToMultiply = variableToMultiply * -1
+                end
+            end
+            for h = i: 1 : length(matrix) %Modifica los valores de esa columna y resuleve los 0 de la matriz%
+                matrix(j,h) = matrix(j,h) + variableToMultiply*matrix(1,h)
+            end
+            results(j) = results(j) + variableToMultiply*results(1)
         end 
     end
-    fHalfL = fHalfL + 1
-    fHalfS = fHalfS - 1
+    fHalfHeight = fHalfHeight + 1
+    fHalfLongitude = fHalfLongitude - 1
 end
+
+sHalfLongitude = 2
+sHalfHeight = length(matrix)-1
+variableToMultiply = 0
+for i = length(matrix) : -1 : sHalfLongitude %Recorre en horizontal porque primero se resuelve hacia abajo%
+    for j = 1 : 1 : sHalfHeight %Recorre hacia abajo%
+        if matrix(j,i) ~= 0
+            if matrix(j,i) > 0 
+                variableToMultiply = -(matrix(j,i))/(matrix(sHalfHeight+1,i))
+                if matrix(j,i) + variableToMultiply*matrix(sHalfHeight+1,i) ~= 0
+                    variableToMultiply = variableToMultiply * -1
+                end
+            else
+                variableToMultiply = (matrix(j,i))/(matrix(sHalfHeight+1,i))
+                if matrix(j,i) + variableToMultiply*matrix(sHalfHeight+1,i) ~= 0
+                    variableToMultiply = variableToMultiply * -1
+                end
+            end
+            for h = i: -1 : 1 %Modifica los valores de esa columna y resuleve los 0 de la matriz%
+                matrix(j,h) = matrix(j,h) + variableToMultiply*matrix(sHalfHeight+1,h)
+            end
+            results(j) = results(j) + variableToMultiply*results(sHalfHeight+1)
+        end 
+    end
+    sHalfHeight = sHalfHeight - 1
+    sHalfLongitude = sHalfLongitude + 1
+end
+disp(matrix)
+disp(results)
